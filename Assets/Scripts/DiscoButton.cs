e32d20d8aa388da110c64dd6464b713825311157
Apo <abdurrahman_usta_41@hotmail.com>
2025-10-22 18:26:35 +0300

    Disco done!

--- Assets/Scripts/DiscoButton.cs
+++ Assets/Scripts/DiscoButton.cs
@@ -9,79 +9,43 @@ public class DiscoButton : baseInteractableItems
     public GameObject canvasAnim;
     public GameObject discoPlane;
     public GameObject particle;
-    public GameObject video;
-
-    Animator _discoBtn;
-    Animator _discoBall;
-    Animator _canvasAnim;
-    Animator _discoPlane;
-
     public bool canUse = true;
+    Animator _discoBtn;
 
     private void Start()
     {
         _discoBtn = discoBtnAnim.GetComponent<Animator>();
-        _discoBall = discoBallAnim.GetComponent<Animator>();
-        _canvasAnim = canvasAnim.GetComponent<Animator>();
-        _discoPlane = discoPlane.GetComponent<Animator>();
-
-        Debug.Log("Video animasyonuna bak");
-        Debug.Log("Animasyonlarý baþlangýç pozisyonlarýna döndür");
     }
 
     public override void OnUse(PlayerStats player)
     {
-        StartDisco();
-        //if (!canUse)
-        //{
-        //    GameManager.instance.ShowMessageText("Tekrar kullanmadan bekle");
-        //    return;
-        //}
-        //canUse = false;
-        //Play();
-    }
+        if (!canUse)
+        {
+            GameManager.instance.ShowMessageText("Tekrar kullanmadan bekle");
+            return;
+        }
+        canUse = false;
+        Play();        
 
-    public void StartDisco()
+        // 57 saniye sonra tekrar aktif hale getir ve objeleri kapat
+        StartCoroutine(PlayerStats.Instance.Cooldown(57f, () =>
+        {            
+            canUse = true;
+            discoBallAnim.SetActive(false);
+            canvasAnim.SetActive(false);
+            discoPlane.SetActive(false);
+            _discoBtn.SetBool("IsDisco", false);
+        }));
+    }
+    public void Play()
     {
-        PlayBallAnimation();
-        PlayBtnAnimation();
-        PlayCanvasAnimation();
-        PlayPlaneAnimation();
+        _discoBtn.SetBool("IsDisco", true);
+        discoPlane.SetActive(true);
+        discoBallAnim.SetActive(true);
+        canvasAnim.SetActive(true);
         particle.SetActive(true);
-    }
-    //public void Play()
-    //{
-    //    PlayBallAnimation();
-    //    PlayerStats.Instance.Cooldown(0.5f, () => canUse = true);
-    //    PlayBtnAnimation();
-    //    PlayerStats.Instance.Cooldown(0.5f, () => canUse = true);
-    //    PlayCanvasAnimation();
-    //    PlayerStats.Instance.Cooldown(0.5f, () => canUse = true);
-    //    PlayPlaneAnimation();
-    //    PlayerStats.Instance.Cooldown(0.5f, () => canUse = true);
-    //    particle.SetActive(true);
-    //    PlayerStats.Instance.Cooldown(10, () => canUse = true);
-    //}
-
-
-    public void PlayBtnAnimation()
-    {
-        _discoBtn.SetTrigger("IsDisco");
-    }
-
-    public void PlayBallAnimation()
-    {
-        _discoBall.SetTrigger("DiscoBall");
-    }
-
-    public void PlayCanvasAnimation()
-    {
-        video.SetActive(true);
-        _canvasAnim.SetTrigger("DiscoVideo");
-    }
-
-    public void PlayPlaneAnimation()
-    {
-        _discoPlane.SetTrigger("DiscoPlane");
+        PlayerStats.Instance.RestSanity(500);
+        PlayerStats.Instance.RestStamina(500);
+        PlayerStats.Instance.EatFood(500);
     }
 }
